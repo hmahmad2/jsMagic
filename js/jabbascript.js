5585256ca54159e16a8628e2df06169a5f657cd9
Hamaad Ahmad <hamaad.m.ahmad@gmail.com>
2019-06-19 11:54:24 -0500

    testing out how values are drawn from forms

--- js/jabbascript.js
+++ js/jabbascript.js
@@ -10,6 +10,18 @@ let tim = new Date();
 // tim = tim.toDateString();
 tim = tim.toLocaleDateString();
 
+// testing how value extraction works
+function valueExtraction() {
+   var calcTest = document.getElementById("testValue").value;
+   console.log(calcTest);
+}
+// document.getElementById("testButton").onclick = function() {
+//    valueExtraction();
+// };
+
+/* So I don't yet get why this commented code up here doesn't work properly, but setting the code to activate on the HTML side worked fine so... ??? This solution is not good for data abstraction, I need to work on getting the commented code to work */
+
+
 
 // Output
 console.log(tim);
